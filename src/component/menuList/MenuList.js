186b33bc659097cda4ffdc05c171adac43b2f751
Leotori <leovid2392@gmail.com>
2021-07-07 09:30:52 -0500

    menus buttons functionality added

--- src/component/menuList/MenuList.js
+++ src/component/menuList/MenuList.js
@@ -1,13 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import menu_vinos from "../../assets/postres_esp.png";
 
 function MenuList({ list }) {
+	const [vinos, setVinos] = useState(false);
+
+	const handleVinos = () => {
+		if (!vinos) {
+			setVinos(true);
+		} else {
+			setVinos(false);
+		}
+	};
+
+	if (vinos) {
+		return (
+			<div>
+				<h1>Nav Bar</h1>
+
+				<button>{list[0]}</button>
+				<button>{list[1]}</button>
+				<button onClick={handleVinos}>{list[2]}</button>
+				<figure>
+					<img src={menu_vinos} alt='menu vinos' />
+				</figure>
+			</div>
+		);
+	}
 	return (
 		<div>
-			<h1>Menu list</h1>
+			<h1>Nav Bar</h1>
 
-			<p>{list[0]}</p>
-			<p>{list[1]}</p>
-			<p>{list[2]}</p>
+			<button>{list[0]}</button>
+			<button>{list[1]}</button>
+			<button onClick={handleVinos}>{list[2]}</button>
 		</div>
 	);
 }
