ce7e5594e6f6be3003da3afcc21f7a4b53eb2b21
Leotori <leovid2392@gmail.com>
2021-09-01 07:13:17 -0500

    september_05

--- src/component/event/Event.js
+++ src/component/event/Event.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 
-import august_25 from "../../assets/slider/august_25.jpeg";
-import august_28 from "../../assets/slider/august_28.jpeg";
+import september_05 from "../../assets/slider/september_05.jpeg";
+// import default_img from "../../assets/slider/default.jpeg";
 
 // import { GrFormPrevious } from "react-icons/gr";
 import { FiChevronLeft } from "react-icons/fi";
@@ -74,15 +74,15 @@ function Event() {
 const events = [
 	{
 		id: 1,
-		url: "https://www.ticketfairy.com/event/francesca-lombardo-28aug2021/?pk=cd0c2ff489c65a3eb7f1836cfaefb35f5a68b0d75c1ddb8b72c3add29d138f8f",
-		title: "august 28 event, Special Dinner",
-		image: august_28,
+		url: "https://www.ticketfairy.com/event/david-penn-5sep2021/",
+		title: "special dinner with david penn, golden hour",
+		image: september_05,
 	},
 	{
 		id: 2,
-		url: "https://www.ticketfairy.com/event/francesca-lombardo-28aug2021/?pk=cd0c2ff489c65a3eb7f1836cfaefb35f5a68b0d75c1ddb8b72c3add29d138f8f",
-		title: "august 28 event, Special Dinner",
-		image: august_28,
+		url: "https://www.ticketfairy.com/event/david-penn-5sep2021/",
+		title: "special dinner with david penn, golden hour",
+		image: september_05,
 	},
 ];
 
