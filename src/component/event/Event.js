c50b3eb72786b8eaa3e394576dff7d70b43fd748
Leotori <leovid2392@gmail.com>
2021-09-02 12:17:44 -0500

    september 10, 12, 16 17 events added

--- src/component/event/Event.js
+++ src/component/event/Event.js
@@ -1,7 +1,11 @@
 import React, { useState, useEffect } from "react";
 
-import september_05 from "../../assets/slider/september_05.jpeg";
 import september_02 from "../../assets/slider/september_02.jpeg";
+import september_05 from "../../assets/slider/september_05.jpeg";
+import september_10 from "../../assets/slider/september_10.jpeg";
+import september_12 from "../../assets/slider/september_12.jpeg";
+import september_16 from "../../assets/slider/september_16.jpeg";
+import september_17 from "../../assets/slider/september_17.jpeg";
 // import default_img from "../../assets/slider/default.jpeg";
 
 // import { GrFormPrevious } from "react-icons/gr";
@@ -84,6 +88,30 @@ const events = [
 		url: "https://www.ticketfairy.com/event/david-penn-5sep2021/",
 		title: "special dinner with david penn, golden hour",
 		image: september_05,
+	},
+	{
+		id: 3,
+		url: "https://www.ticketfairy.com/event/matthias-meyer-11sep2021/",
+		title: "special dinner with mathias meyer,golden hour",
+		image: september_10,
+	},
+	{
+		id: 4,
+		url: "https://www.ticketfairy.com/event/marino-canal-12sep2021/",
+		title: "special dinner with marino canal, neo human",
+		image: september_12,
+	},
+	{
+		id: 5,
+		url: "https://www.ticketfairy.com/event/hunter-game-16sep2021/",
+		title: "special dinner with hunter/game",
+		image: september_16,
+	},
+	{
+		id: 6,
+		url: "https://www.ticketfairy.com/event/nicole-moudaber-17sep2021/",
+		title: "special dinner with nicole moudaber, victoria engel",
+		image: september_17,
 	},
 ];
 
